bd018cb06adf8198f9008748eae91099a3efb559
Maike <maikonalexandre574@gmail.com>
2024-02-27 13:58:14 -0300

    feat: add new component table

--- packages/frontend/src/pages/app/dashboard.tsx
+++ packages/frontend/src/pages/app/dashboard.tsx
@@ -1,17 +1,60 @@
+import { Pagination } from '@/components/pagination'
 import { Calendar } from '@/components/ui/calendar'
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from '@/components/ui/table'
 
 export function Dashboard() {
   return (
-    <div className="flex gap-2 px-2">
+    <div className="flex gap-2 px-3">
       <div className="space-y-2">
         <Calendar className="rounded bg-zinc-800" />
 
-        <div className="bg-zinc-800 p-2">1</div>
-        <div className="bg-zinc-800 p-2">1</div>
-        <div className="bg-zinc-800 p-2">1</div>
-        <div className="bg-zinc-800 p-2">1</div>
+        <div className="rounded bg-zinc-800 p-2">1</div>
+        <div className="rounded bg-zinc-800 p-2">1</div>
+        <div className="rounded bg-zinc-800 p-2">1</div>
+        <div className="rounded bg-zinc-800 p-2">1</div>
       </div>
-      <div>catag</div>
+
+      <div className="flex w-full flex-col rounded bg-zinc-800 p-4">
+        <Pagination currentDate={new Date()} />
+
+        <div className="w-full ">
+          <Table className="w-full">
+            <TableHeader>
+              <TableRow>
+                <TableHead className="w-[64px]"></TableHead>
+                <TableHead>Segunda</TableHead>
+                <TableHead>Terça</TableHead>
+                <TableHead>Quarta</TableHead>
+                <TableHead>Quinta</TableHead>
+                <TableHead>Sexta</TableHead>
+                <TableHead>Sábado</TableHead>
+                <TableHead>Domingo</TableHead>
+              </TableRow>
+            </TableHeader>
+            <TableBody>
+              {[1, 2, 3, 4, 5, 6, 7].map((invoice) => (
+                <TableRow className="" key={invoice}>
+                  <TableCell className="p-3 text-sm">{invoice}</TableCell>
+                  <TableCell className="p-3 text-sm"></TableCell>
+                  <TableCell className="p-3 text-sm"></TableCell>{' '}
+                  <TableCell className="p-3 text-sm"></TableCell>{' '}
+                  <TableCell className="p-3 text-sm"></TableCell>{' '}
+                  <TableCell className="p-3 text-sm"></TableCell>{' '}
+                  <TableCell className="p-3 text-sm"></TableCell>{' '}
+                  <TableCell></TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </div>
+      </div>
     </div>
   )
 }
