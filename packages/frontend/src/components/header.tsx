eea7a182126a83780df36371b1a11cc6868c6079
Maike <maikonalexandre574@gmail.com>
2024-02-27 10:38:30 -0300

    feat: create basic header component

--- packages/frontend/src/components/header.tsx
+++ packages/frontend/src/components/header.tsx
@@ -1,7 +1,16 @@
+import { Calendar, User } from 'lucide-react'
+
 export function Header() {
   return (
-    <div className="flex w-full">
-      <div>Dashboard</div>
+    <div className="flex w-full items-center justify-between px-6 py-3">
+      <div className="flex items-center gap-2">
+        <Calendar size={20} />
+        <span className="text-lg font-semibold">Dashboard</span>
+      </div>
+      <div className="flex items-center gap-2">
+        <span className="text-sm">usename</span>
+        <User size={20} />
+      </div>
     </div>
   )
 }
