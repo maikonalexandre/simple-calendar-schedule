877e420f91666be7fce5d15a122536cd2482ef22
Maike <maikonalexandre574@gmail.com>
2024-02-27 10:39:03 -0300

    feat: create basic main layout

--- packages/frontend/src/pages/_layouts/main.tsx
+++ packages/frontend/src/pages/_layouts/main.tsx
@@ -4,9 +4,14 @@ import { Header } from '@/components/header'
 
 export function MainLayout() {
   return (
-    <div className="w-full px-4">
-      <Header />
-      <Outlet />
+    <div className="h-full w-full bg-zinc-900 text-zinc-50">
+      <div className="flex h-full">
+        <div className="h-screen w-1 bg-gradient-to-t from-green-300 via-blue-500 to-purple-600"></div>
+        <div className="h-full w-full">
+          <Header />
+          <Outlet />
+        </div>
+      </div>
     </div>
   )
 }
