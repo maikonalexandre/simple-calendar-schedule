23faf12c03009cae09ae9652143b70bf322c578c
Maike <maikonalexandre574@gmail.com>
2024-02-27 10:39:16 -0300

    feat: create dashboard page

--- packages/frontend/src/pages/app/dashboard.tsx
+++ packages/frontend/src/pages/app/dashboard.tsx
@@ -2,8 +2,16 @@ import { Calendar } from '@/components/ui/calendar'
 
 export function Dashboard() {
   return (
-    <div className="bg-zinc-900">
-      <Calendar className="text-white" />
+    <div className="flex gap-2 px-2">
+      <div className="space-y-2">
+        <Calendar className="rounded bg-zinc-800" />
+
+        <div className="bg-zinc-800 p-2">1</div>
+        <div className="bg-zinc-800 p-2">1</div>
+        <div className="bg-zinc-800 p-2">1</div>
+        <div className="bg-zinc-800 p-2">1</div>
+      </div>
+      <div>catag</div>
     </div>
   )
 }
