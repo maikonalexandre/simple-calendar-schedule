d9cecf52b9b9b8a2665f838463ea0ccf338b5fec
Maike <maikonalexandre574@gmail.com>
2024-02-27 18:28:05 -0300

    style: remove unused imports

--- packages/frontend/src/pages/app/dashboard.tsx
+++ packages/frontend/src/pages/app/dashboard.tsx
@@ -8,10 +8,10 @@ export function Dashboard() {
   return (
     <div className="flex gap-2 px-3">
       <div className="space-y-2">
-        <Calendar className="rounded bg-zinc-800" locale={ptBR} />
+        <Calendar className="rounded bg-card" locale={ptBR} />
       </div>
 
-      <div className="flex w-full flex-col gap-4 rounded bg-zinc-800 p-4">
+      <div className="flex w-full flex-col gap-4 rounded bg-card p-4">
         <Pagination />
         <CalendarTable />
       </div>
