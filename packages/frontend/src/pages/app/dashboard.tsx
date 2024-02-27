2cd7d988adafa468da9f74f2f7d43d847187016f
Maike <maikonalexandre574@gmail.com>
2024-02-27 17:19:10 -0300

    feat: add calendarTable component

--- packages/frontend/src/pages/app/dashboard.tsx
+++ packages/frontend/src/pages/app/dashboard.tsx
@@ -1,15 +1,8 @@
 import { ptBR } from 'date-fns/locale'
 
+import { CalendarTable } from '@/components/calendarTable'
 import { Pagination } from '@/components/pagination'
 import { Calendar } from '@/components/ui/calendar'
-import {
-  Table,
-  TableBody,
-  TableCell,
-  TableHead,
-  TableHeader,
-  TableRow,
-} from '@/components/ui/table'
 
 export function Dashboard() {
   return (
@@ -18,49 +11,9 @@ export function Dashboard() {
         <Calendar className="rounded bg-zinc-800" locale={ptBR} />
       </div>
 
-      <div className="flex w-full flex-col rounded bg-zinc-800 p-4">
+      <div className="flex w-full flex-col gap-4 rounded bg-zinc-800 p-4">
         <Pagination />
-
-        <Table className="w-full">
-          <TableHeader>
-            <TableRow className="grid grid-cols-7 items-center align-middle">
-              {/* <TableHead></TableHead> */}
-              <TableHead>
-                <div>
-                  <span>Seg</span>
-                </div>
-                <div>
-                  <span>24</span>
-                </div>
-              </TableHead>
-              <TableHead>Terça</TableHead>
-              <TableHead>Quarta</TableHead>
-              <TableHead>Quinta</TableHead>
-              <TableHead>Sexta</TableHead>
-              <TableHead>Sábado</TableHead>
-              <TableHead>Domingo</TableHead>
-            </TableRow>
-          </TableHeader>
-          <TableBody>
-            {[1, 8, 9, 2, 3, 4, 5, 6, 7].map((invoice) => (
-              <TableRow
-                key={invoice}
-                className="grid grid-cols-7 items-center justify-items-center align-middle"
-              >
-                {/* <TableCell className="flex h-full w-full p-3 text-sm">
-                  {invoice}
-                </TableCell> */}
-                <TableCell className="flex h-full w-full p-3 text-sm hover:bg-foreground"></TableCell>
-                <TableCell className="flex h-full w-full p-3 text-sm hover:bg-foreground"></TableCell>{' '}
-                <TableCell className="flex h-full w-full p-3 text-sm hover:bg-foreground"></TableCell>{' '}
-                <TableCell className="flex h-full w-full p-3 text-sm hover:bg-foreground"></TableCell>{' '}
-                <TableCell className="flex h-full w-full p-3 text-sm hover:bg-foreground"></TableCell>{' '}
-                <TableCell className="flex h-full w-full p-3 text-sm hover:bg-foreground"></TableCell>{' '}
-                <TableCell></TableCell>
-              </TableRow>
-            ))}
-          </TableBody>
-        </Table>
+        <CalendarTable />
       </div>
     </div>
   )
