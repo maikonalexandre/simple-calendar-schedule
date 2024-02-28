a2c0cc11454a41209aa8d5484d45c3c45404bdb8
Maike <maikonalexandre574@gmail.com>
2024-02-28 11:45:09 -0300

    feat: create datatable component

---
+++ packages/frontend/src/components/datatable.tsx
@@ -0,0 +1,91 @@
+import {
+  ColumnDef,
+  flexRender,
+  getCoreRowModel,
+  useReactTable,
+} from '@tanstack/react-table'
+
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from '@/components/ui/table'
+import { usePagination } from '@/hooks/usePagination'
+import { getWeek } from '@/utils'
+
+interface DataTableProps<TData, TValue> {
+  columns: ColumnDef<TData, TValue>[]
+  data: TData[]
+}
+
+export function DataTable<TData, TValue>({
+  columns,
+  data,
+}: DataTableProps<TData, TValue>) {
+  const { week } = usePagination()
+
+  const t = getWeek(week.startDate, week.endDate).map((e) => {
+    return {
+      accessorKey: e.day,
+      header: `${e.dayOfTheWeek} ${e.day}`,
+    }
+  })
+
+  const table = useReactTable({
+    data,
+    columns: t,
+    getCoreRowModel: getCoreRowModel(),
+  })
+
+  return (
+    <Table>
+      <TableHeader>
+        {table.getHeaderGroups().map((headerGroup) => (
+          <TableRow key={headerGroup.id}>
+            {headerGroup.headers.map((header) => {
+              return (
+                <TableHead key={header.id}>
+                  {header.isPlaceholder
+                    ? null
+                    : flexRender(
+                        header.column.columnDef.header,
+                        header.getContext(),
+                      )}
+                </TableHead>
+              )
+            })}
+          </TableRow>
+        ))}
+      </TableHeader>
+      <TableBody>
+        {table.getRowModel().rows?.length ? (
+          table.getRowModel().rows.map((row) => (
+            <TableRow
+              key={row.id}
+              data-state={row.getIsSelected() && 'selected'}
+            >
+              {row.getVisibleCells().map((cell) => (
+                <TableCell className="hover:bg-blue-500" key={cell.id}>
+                  {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                </TableCell>
+              ))}
+            </TableRow>
+          ))
+        ) : (
+          <TableRow>
+            {Array.from({ length: 7 }).map((_, i) => (
+              <TableCell
+                title="Adicionar evento"
+                className="hover:bg-zinc-700"
+                key={i}
+              />
+            ))}
+          </TableRow>
+        )}
+      </TableBody>
+    </Table>
+  )
+}
