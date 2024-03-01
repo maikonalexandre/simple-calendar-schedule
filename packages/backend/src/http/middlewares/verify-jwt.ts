564ddbbcdc40762f27b4f4235ade6e63ac6c5213
Maike <maikonalexandre574@gmail.com>
2024-02-29 21:50:37 -0300

    feat: add expire time to jwt

--- packages/backend/src/http/middlewares/verify-jwt.ts
+++ packages/backend/src/http/middlewares/verify-jwt.ts
@@ -1,4 +1,6 @@
 import { FastifyReply, FastifyRequest } from 'fastify'
+import { addWeeks, isAfter } from 'date-fns'
+import { timestampToDate } from '../../utils'
 
 export const verifyJwt = async (
   request: FastifyRequest,
@@ -6,6 +8,13 @@ export const verifyJwt = async (
 ) => {
   try {
     await request.jwtVerify()
+
+    const createdAt = request.user.iat
+    const expiredDate = addWeeks(timestampToDate(createdAt), 1)
+
+    if (isAfter(new Date(), expiredDate)) {
+      reply.status(401).send({ message: 'Unauthorized' })
+    }
   } catch (error) {
     reply.status(401).send({ message: 'Unauthorized' })
   }
