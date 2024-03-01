1cefab9c4f69afee6eedb91bf122d7eefe2501b9
Maike <maikonalexandre574@gmail.com>
2024-03-01 09:30:08 -0300

    feat: add search user  by id

--- packages/backend/src/repositories/prisma/prisma-users-repository.ts
+++ packages/backend/src/repositories/prisma/prisma-users-repository.ts
@@ -20,4 +20,14 @@ export class PrismaUsersRepository implements UsersRepository {
 
     return user
   }
+
+  async findById(id: string) {
+    const user = await prisma.user.findFirst({
+      where: {
+        id,
+      },
+    })
+
+    return user
+  }
 }
