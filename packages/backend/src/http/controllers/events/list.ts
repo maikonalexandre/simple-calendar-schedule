3a568ed3adfa4931fcb39ce36056491b4f2c5b57
Maike <maikonalexandre574@gmail.com>
2024-03-01 15:37:24 -0300

    feat: add list events route

--- packages/backend/src/http/controllers/events/list.ts
+++ packages/backend/src/http/controllers/events/list.ts
@@ -2,12 +2,17 @@ import { FastifyRequest, FastifyReply } from 'fastify'
 import { z } from 'zod'
 import { PrimaEventsRepository } from '../../../repositories/prisma/prisma-events-repository'
 import { ListEventsUseCase } from '../../../use-cases/list-events'
+import { getWeekInterval } from '../../../utils'
+
+import { format, isAfter } from 'date-fns'
 
 export async function listEvents(request: FastifyRequest, reply: FastifyReply) {
-  const list = z.object({
-    endDate: z.coerce.date(),
-    startedDate: z.coerce.date(),
-  })
+  const list = z
+    .object({
+      endDate: z.coerce.date(),
+      startedDate: z.coerce.date(),
+    })
+    .refine(({ endDate, startedDate }) => isAfter(endDate, startedDate))
 
   const { endDate, startedDate } = list.parse(request.query)
 
@@ -20,6 +25,21 @@ export async function listEvents(request: FastifyRequest, reply: FastifyReply) {
       startedDate,
       userId: request.user.sub,
     })
+
+    const daysInterval = getWeekInterval(startedDate, endDate)
+
+    if (events && events.length > 1) {
+      const refactoredEventsByDaysInterval = daysInterval.map((day) => {
+        const dayEvents = events.filter(
+          (event) =>
+            format(event.startedAt, 'yyyy-MM-dd') === format(day, 'yyyy-MM-dd'),
+        )
+
+        return { day, eventos: dayEvents }
+      })
+
+      reply.code(200).send({ message: refactoredEventsByDaysInterval })
+    }
 
     reply.status(200).send({ message: events })
   } catch (error) {
