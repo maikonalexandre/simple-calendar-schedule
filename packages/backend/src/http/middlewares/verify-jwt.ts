454e3a3b1d58c4ee25db0344e03475f07c720810
Maike <maikonalexandre574@gmail.com>
2024-03-01 09:29:46 -0300

    feat: add jwt verify hook

--- packages/backend/src/http/middlewares/verify-jwt.ts
+++ packages/backend/src/http/middlewares/verify-jwt.ts
@@ -1,6 +1,6 @@
 import { FastifyReply, FastifyRequest } from 'fastify'
-import { addWeeks, isAfter } from 'date-fns'
-import { timestampToDate } from '../../utils'
+// import { addWeeks, isAfter } from 'date-fns'
+// import { timestampToDate } from '../../utils'
 
 export const verifyJwt = async (
   request: FastifyRequest,
@@ -9,12 +9,12 @@ export const verifyJwt = async (
   try {
     await request.jwtVerify()
 
-    const createdAt = request.user.iat
-    const expiredDate = addWeeks(timestampToDate(createdAt), 1)
+    // const createdAt = request.user.iat
+    // const expiredDate = addWeeks(timestampToDate(createdAt), 1)
 
-    if (isAfter(new Date(), expiredDate)) {
-      reply.status(401).send({ message: 'Unauthorized' })
-    }
+    // if (isAfter(new Date(), expiredDate)) {
+    //   reply.status(401).send({ message: 'Unauthorized' })
+    // }
   } catch (error) {
     reply.status(401).send({ message: 'Unauthorized' })
   }
