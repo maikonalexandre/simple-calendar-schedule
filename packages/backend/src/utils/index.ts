5989162d1cedaf0d734a955d9782a5006f962312
Maike <maikonalexandre574@gmail.com>
2024-03-01 15:38:43 -0300

    feat: add get days interval  fn

--- packages/backend/src/utils/index.ts
+++ packages/backend/src/utils/index.ts
@@ -1,5 +1,12 @@
-import { format, fromUnixTime } from 'date-fns'
+import { format, fromUnixTime, eachDayOfInterval } from 'date-fns'
 
 export function timestampToDate(timestamp: EpochTimeStamp) {
   return format(fromUnixTime(timestamp), 'yyyy-MM-dd HH:mm:ss')
+}
+
+export function getWeekInterval(start: Date, end: Date) {
+  return eachDayOfInterval({
+    start,
+    end,
+  })
 }
