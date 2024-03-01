dd02fe91ff98d0c114d16e32f5bf2561f046ff69
Maike <maikonalexandre574@gmail.com>
2024-03-01 15:38:27 -0300

    feat: add health route

--- packages/backend/src/http/controllers/users/routes.ts
+++ packages/backend/src/http/controllers/users/routes.ts
@@ -1,8 +1,11 @@
 import { FastifyInstance } from 'fastify'
 import { users } from './users'
 import { authenticate } from './authenticate'
+import { health } from './health'
 
 export async function appRoutes(app: FastifyInstance) {
   app.post('/users', users)
   app.post('/sessions', authenticate)
+
+  app.get('/health', health)
 }
