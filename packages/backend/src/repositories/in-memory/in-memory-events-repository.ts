6fc06f380e56b3295e565bec84375fdaedb87a8d
Maike <maikonalexandre574@gmail.com>
2024-03-01 19:27:44 -0300

    feat: create in memory repositories

---
+++ packages/backend/src/repositories/in-memory/in-memory-events-repository.ts
@@ -0,0 +1,121 @@
+import { Event } from '@prisma/client'
+import { EventsRepository } from '../events-repository'
+
+interface createEventProps {
+  name: string
+  date: Date
+  startedAt: Date
+  finalizedAt: Date
+  description: string
+  userId: string
+}
+
+interface updateEventProps {
+  id: string
+  name: string
+  date: Date
+  startedAt: Date
+  finalizedAt: Date
+  description: string
+  userId: string
+}
+
+export class InMemoryEventsRepository implements EventsRepository {
+  events: Event[] = []
+  async create(data: createEventProps) {
+    const event = {
+      id: 'event',
+      name: data.name,
+      date: data.date,
+      startedAt: data.startedAt,
+      finalizedAt: data.finalizedAt,
+      userId: data.userId,
+      description: data.description,
+    }
+
+    this.events.push()
+
+    return event
+  }
+
+  async findByDateInterval(data: {
+    userId: string
+    startedAt: Date
+    finalizedAt: Date
+  }) {
+    const event =
+      this.events.find((event) => {
+        if (event.userId === data.userId) {
+          if (
+            event.startedAt <= data.startedAt &&
+            event.finalizedAt >= data.startedAt
+          ) {
+            return event
+          }
+
+          if (
+            event.startedAt <= data.finalizedAt &&
+            event.finalizedAt >= data.finalizedAt
+          ) {
+            return event
+          }
+
+          if (
+            event.startedAt >= data.startedAt &&
+            event.finalizedAt <= data.finalizedAt
+          ) {
+            return event
+          }
+        }
+
+        return null
+      }) || null
+
+    return event
+  }
+
+  async delete(data: { id: string; userId: string }) {
+    const event = {
+      id: data.id,
+      name: 'event',
+      date: new Date(),
+      startedAt: new Date(),
+      finalizedAt: new Date(),
+      userId: data.userId,
+      description: 'description',
+    }
+
+    return event
+  }
+
+  async findById(data: { id: string; userId: string }) {
+    const event = this.events.find((event) => event.id === data.id) || null
+
+    return event
+  }
+
+  async findAllByUserId(data: {
+    userId: string
+    startedDate: Date
+    endDate: Date
+  }) {
+    const event =
+      this.events.filter((event) => event.userId === data.userId) || []
+
+    return event
+  }
+
+  async updateEvent(data: updateEventProps) {
+    const event = {
+      id: data.id,
+      name: data.name,
+      date: data.date,
+      startedAt: data.startedAt,
+      finalizedAt: data.finalizedAt,
+      userId: data.userId,
+      description: data.description,
+    }
+
+    return event
+  }
+}
--- packages/backend/src/repositories/in-memory/in-memory-users-repository.ts
+++ packages/backend/src/repositories/in-memory/in-memory-users-repository.ts
@@ -1,12 +1,31 @@
-// import { Prisma, User } from '@prisma/client'
-// import { UsersRepository } from '../users-repository'
+import { Prisma, User } from '@prisma/client'
+import { UsersRepository } from '../users-repository'
 
-// export class InMemoryUserRepository implements UsersRepository {
-//   public users: Prisma.UserCreateInput[] = []
-//   async create(data: Prisma.UserCreateInput) {
-//     return new Promise<User>((resolve) => {
-//       this.users.push(data)
-//       resolve(data)
-//     })
-//   }
-// }
+export class InMemoryUserRepository implements UsersRepository {
+  users: User[] = []
+
+  async findByEmail(email: string) {
+    const user = this.users.find((user) => email === user.email) || null
+
+    return user
+  }
+
+  async findById(id: string) {
+    const user = this.users.find((user) => id === user.id) || null
+
+    return user
+  }
+
+  async create(data: Prisma.UserCreateInput) {
+    const user = {
+      id: 'userId',
+      email: data.email,
+      name: data.name,
+      password_hash: data.password_hash,
+    }
+
+    this.users.push(user)
+
+    return user
+  }
+}
