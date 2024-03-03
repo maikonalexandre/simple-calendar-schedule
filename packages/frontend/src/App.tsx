9a36816108d0de10d900c0bdeab76caf252c5add
Maike <maikonalexandre574@gmail.com>
2024-03-03 20:06:46 -0300

    feat: add react query dev tools

--- packages/frontend/src/App.tsx
+++ packages/frontend/src/App.tsx
@@ -1,10 +1,12 @@
 import './global.css'
 
 import { QueryClientProvider } from '@tanstack/react-query'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import { RouterProvider } from 'react-router-dom'
 import { Toaster } from 'sonner'
 
 import { ThemeProvider } from './components/themeProvider'
+import { env } from './config/env'
 import { queryClient } from './config/react-query'
 import { AuthProvider } from './hooks/useAuth'
 import { router } from './routes'
@@ -13,6 +15,10 @@ function App() {
   return (
     <ThemeProvider>
       <QueryClientProvider client={queryClient}>
+        {env.MODE === 'development' && (
+          <ReactQueryDevtools initialIsOpen={false} />
+        )}
+
         <AuthProvider>
           <Toaster richColors theme="dark" position="top-right" />
           <RouterProvider router={router} />
